256054086052bb7328ff645537bbca22dbd416cc
Luffy-Jpg <138620598+Luffy-Jpg@users.noreply.github.com>
2025-04-30 19:16:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const music = document.getElementById('bg-music');
-  music.volume = 0.4;
-});
+const bgMusic = document.getElementById('bg-music');
+
+function toggleMusic() {
+  if (bgMusic.paused) {
+    bgMusic.play();
+  } else {
+    bgMusic.pause();
+  }
+}
