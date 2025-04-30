cae937b924429823e891a96b0c999c7d6315f905
Luffy-Jpg <138620598+Luffy-Jpg@users.noreply.github.com>
2025-04-30 19:06:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-// This can be used later for enhancements (like music toggle)
 document.addEventListener('DOMContentLoaded', () => {
   const music = document.getElementById('bg-music');
-  music.volume = 0.5; // Optional: adjust volume
+  music.volume = 0.4;
 });
